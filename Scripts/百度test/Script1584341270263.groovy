478d06e95eb37b4876f4359bbccf2c912521782d
chadz1 <chadz1@163.com>
2020-03-16 15:07:09 +0800

    测试git

---
+++ Scripts/百度test/Script1584341270263.groovy
@@ -0,0 +1,31 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+
+'打开浏览器输入网址'
+WebUI.openBrowser('www.baidu.com')
+
+'屏幕最大化'
+WebUI.maximizeWindow()
+
+'输入搜索值“大时信息科技有限公司”'
+WebUI.sendKeys(findTestObject('baidu/百度输入框'), '大时信息科技有限公司')
+
+'点击百度一下按钮'
+WebUI.click(findTestObject('baidu/百度一下'))
+
+WebUI.closeBrowser()
+
